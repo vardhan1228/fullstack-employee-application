6b07873aa33891a6acac36b74698a08b38c2ea18
vardhan1228 <vvardhan1211@gmail.com>
2025-06-12 12:28:59 +0530

    vsv

--- employeemanagement-frontend/src/service/EmployeeService.js
+++ employeemanagement-frontend/src/service/EmployeeService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const BASE_URL = process.env.REACT_APP_BACKEND_URL
-const BASE_URL = "http://38.06.37.4:8080/employee";
+const BASE_URL = "http://veera.narni.co.in/employee";    //For kubernetes deployment
+//const BASE_URL = "http://38.06.37.4:8080/employee";      // For server deployment on aws   
 // const BASE_URL = process.env.REACT_APP_BASE_URL ; // Fallback URL
 class EmployeeService{
 
